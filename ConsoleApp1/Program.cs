6459271bd1ac30e1767f7f2b0e12d8ecc37bccfa
Emre <emre_toglu@hotmail.com>
2020-02-22 14:49:57 +0100

    Simple guessing game

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        const int maxTrials = 5;
+
+        static void Main(string[] args)
+        {            
+            Console.WriteLine("Guess the name I'm thinking of");
+            Console.WriteLine("It starts with g");
+
+            string correctName = "gertrud";
+
+            for (int i = 0; i < maxTrials; i++)
+            {
+                string guessedName = Console.ReadLine();
+
+                if (guessedName == correctName)
+                {
+                    Console.WriteLine("Congratulations, You guessed it!");
+                    Thread.Sleep(1300);
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine("Wrong name, try again! tries left: " + (4 - i));
+                }
+            }
+            
+        }
+    }
+}
